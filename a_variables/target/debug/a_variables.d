e88d976fdca8f49c5a80f8eace54f46c218a0f4b
Manish <darksalterego99@gmail.com>
2024-11-11 21:12:38 +0530

    Remove generated files and build artifacts from a_variables crate

---
+++ a_variables/target/debug/a_variables.d
@@ -0,0 +1,1 @@
+C:\Users\slaye\SudoMaverick\RustyCargo\a_variables\target\debug\a_variables.exe: C:\Users\slaye\SudoMaverick\RustyCargo\a_variables\src\main.rs
---
+++ a_variables/target/debug/deps/a_variables.d
@@ -0,0 +1,5 @@
+C:\Users\slaye\SudoMaverick\RustyCargo\a_variables\target\debug\deps\a_variables.exe: src/main.rs
+
+C:\Users\slaye\SudoMaverick\RustyCargo\a_variables\target\debug\deps\a_variables.d: src/main.rs
+
+src/main.rs:
