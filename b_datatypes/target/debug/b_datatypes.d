986053f6fb04daf737e9fef3ec452392910194eb
manish <darksalterego99@gmail.com>
2025-03-18 15:08:00 +0530

    Remove generated files and build artifacts from b_datatypes crate

---
+++ b_datatypes/target/debug/b_datatypes.d
@@ -0,0 +1,1 @@
+/home/keir/rustycargo/rustyplay/b_datatypes/target/debug/b_datatypes: /home/keir/rustycargo/rustyplay/b_datatypes/src/main.rs
---
+++ b_datatypes/target/debug/deps/b_datatypes-2f9ce53ed43e4b0a.d
@@ -0,0 +1,5 @@
+/home/keir/rustycargo/rustyplay/b_datatypes/target/debug/deps/b_datatypes-2f9ce53ed43e4b0a: src/main.rs
+
+/home/keir/rustycargo/rustyplay/b_datatypes/target/debug/deps/b_datatypes-2f9ce53ed43e4b0a.d: src/main.rs
+
+src/main.rs:
